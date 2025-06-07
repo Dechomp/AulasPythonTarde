148c3b12b4b9849202e5e0cafc462d499f02e050
Dechomp <112034108+Dechomp@users.noreply.github.com>
2025-06-07 14:16:40 -0300

    Add files via upload

---
+++ VariaveisTarde.py
@@ -0,0 +1,12 @@
+def main():
+  nomeEscola = "Carlos Alberto"
+  idade = 17
+  tamanho = 1.55
+  sexo = 'M'
+
+  print(nomeEscola)
+  print(idade)
+  print(tamanho)
+  print(sexo)
+  return 0
+main()
